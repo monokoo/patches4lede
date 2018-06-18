e3935e3585028ccf2e0823ba2b7d643b93bf2835
monokoo <realstones2012@gmail.com>
2018-06-19 00:02:32 +0800

    update

--- apply_patch.sh
+++ apply_patch.sh
@@ -39,6 +39,7 @@ echo "Everything is done!"
 cd /home/lede/lede-source
 patch -p1 -i /home/lede/patches4lede/personal-modify.patch
 [ -n "$(cat /home/lede/lede-source/.config | grep phicomm-k3)" ] && {
+	echo "apply patches for K3..."
 	patch -p1 -i /home/lede/patches4lede/just_for_k3.patch
 }
 
