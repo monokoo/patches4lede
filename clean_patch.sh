8fb75c07f7ccd1126db8c28ba2d3ca3fbbd2ee11
monokoo <realstones2012@gmail.com>
2018-06-22 20:15:52 +0800

    update

--- clean_patch.sh
+++ clean_patch.sh
@@ -7,7 +7,6 @@ git checkout -- package/network/ipv6/6in4/files/6in4.sh
 git checkout -- package/base-files/files/etc/rc.button/reset
 git checkout -- package/network/config/firewall/files/firewall.init
 
-rm -f package/kernel/mac80211/patches/970-0001-ath10k_add-LED-and-GPIO-controlling-support-for-various-chipsets.patch
 rm -f package/kernel/mac80211/patches/970-0002-ath10k_add-LED-and-GPIO-controlling-support-for-various-chipsets-kcompat.patch
 
 
