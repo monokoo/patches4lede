208b30eaa9e1cddd6a64ec412504d74ebae03303
monokoo <realstones2012@gmail.com>
2018-07-03 23:59:37 +0800

    update

--- apply_patch.sh
+++ apply_patch.sh
@@ -7,3 +7,8 @@ patch -p1 -i /home/lede/patches4lede/personal-modify.patch
 	patch -p1 -i /home/lede/patches4lede/just_for_k3.patch
 }
 
+[ -n "$(cat /home/lede/lede-source/.config | grep CONFIG_PACKAGE_minidlna=y)" ] && {
+	echo "disselect PACKAGE minidlna..."
+	sed -i 's/CONFIG_PACKAGE_minidlna=y/# CONFIG_PACKAGE_minidlna is not set/' /home/lede/lede-source/.config
+}
+
