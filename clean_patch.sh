e1ad19fbbb8fa6b238db2f3604be7266049731ed
monokoo <realstones2012@gmail.com>
2018-09-08 18:59:31 +0800

    update

--- clean_patch.sh
+++ clean_patch.sh
@@ -36,6 +36,8 @@ git checkout -- applications/luci-app-minidlna/Makefile
 git checkout -- applications/luci-app-ntpc/luasrc/model/cbi/ntpc/ntpc.lua
 git checkout -- modules/luci-base/luasrc/view/sysauth.htm
 git checkout -- modules/luci-base/luasrc/view/cbi/apply_widget.htm
+git checkout -- modules/luci-mod-admin-full/luasrc/view/admin_status/connections.htm
+git checkout -- modules/luci-base/luasrc/sys.lua
 git checkout -- modules/luci-base/root/etc/config/luci
 git checkout -- protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_pppoe.lua
 git checkout -- applications/luci-app-nlbwmon/luasrc/controller/nlbw.lua
