613211f31733205826f9beea167621ed83e889e0
monokoo <monokoo@users.noreply.github.com>
2018-10-03 12:17:59 +0800

    update

--- clean_patch.sh
+++ clean_patch.sh
@@ -18,6 +18,7 @@ git checkout -- net/aria2/files/aria2.conf
 git checkout -- net/nlbwmon/files/nlbwmon.init
 #git checkout -- net/usbip/patches-2.0/100-musl-compat.patch
 git checkout -- net/haproxy/Makefile
+git checkout -- net/nginx/Makefile
 git checkout -- net/nginx/files-luci-support/luci_nginx_ssl.conf
 git checkout -- net/nginx/files-luci-support/luci_nginx.conf
 git checkout -- net/uwsgi-cgi/files-luci-support/uwsgi.conf
