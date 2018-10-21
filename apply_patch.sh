a2258faf6639f1ba252553b3a0c98e421de45ec3
monokoo <monokoo@users.noreply.github.com>
2018-10-21 16:00:04 +0800

    update

--- apply_patch.sh
+++ apply_patch.sh
@@ -8,6 +8,17 @@ patch -p1 -i /home/lede/patches4lede/personal-modify.patch
 	#patch -p1 -i /home/lede/patches4lede/mac80211-patches-k3.patch
 }
 
+if [ -n "$(cat /home/lede/lede-source/.config | grep x86_64)" ]; then
+	#echo "apply patches for x86_64..."
+	#patch -p1 -i /home/lede/patches4lede/just_for_x64.patch
+	#patch -p1 -i /home/lede/patches4lede/mac80211-patches-k3.patch
+else
+	echo "apply patches for other archs..."
+	patch -p1 -i /home/lede/patches4lede/just_for_other_arch.patch
+
+fi
+
+
 [ -n "$(cat /home/lede/lede-source/.config | grep CONFIG_PACKAGE_minidlna=y)" ] && {
 	echo "disselect PACKAGE minidlna..."
 	sed -i 's/CONFIG_PACKAGE_minidlna=y/# CONFIG_PACKAGE_minidlna is not set/' /home/lede/lede-source/.config
