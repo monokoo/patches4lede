7c01579dafa7ad43d1726757a0e99f2fa035e44f
monokoo <monokoo@users.noreply.github.com>
2018-10-21 16:01:47 +0800

    fix

--- apply_patch.sh
+++ apply_patch.sh
@@ -9,7 +9,7 @@ patch -p1 -i /home/lede/patches4lede/personal-modify.patch
 }
 
 if [ -n "$(cat /home/lede/lede-source/.config | grep x86_64)" ]; then
-	#echo "apply patches for x86_64..."
+	echo "apply patches for x86_64..."
 	#patch -p1 -i /home/lede/patches4lede/just_for_x64.patch
 	#patch -p1 -i /home/lede/patches4lede/mac80211-patches-k3.patch
 else
